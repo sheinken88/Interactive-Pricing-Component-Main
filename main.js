d21a391eebd7d5304d65b2d634766510bd75dddc
sheinken <heinken.sebastian@gmail.com>
2022-10-13 14:43:27 -0300

    new file:   .gitignore 	new file:   README-template.md 	modified:   README.md 	new file:   design/active-states.jpg 	new file:   design/desktop-design.jpg 	new file:   design/desktop-preview.jpg 	new file:   design/mobile-design.jpg 	new file:   images/bg-pattern.svg 	new file:   images/favicon-32x32.png 	new file:   images/icon-check.svg 	new file:   images/icon-slider.svg 	new file:   images/pattern-circles.svg 	new file:   index.html 	new file:   main.js 	new file:   style-guide.md 	new file:   style.css

---
+++ main.js
@@ -0,0 +1,28 @@
+let view = document.getElementById("pageView");
+let price = document.getElementById("price");
+let slider = document.getElementById("priceSlider");
+let toggle = document.getElementById("billing");
+let pageViews = ["10k", "50k", "100k", "500k", "1M"];
+let perMonth = [8, 12, 16, 24, 36];
+let isYearly = false;
+
+toggle.addEventListener("change", () => {
+  if (isYearly === false) {
+    isYearly = true;
+  } else {
+    isYearly = false;
+  }
+});
+
+function updateValue() {
+  if (isYearly) {
+    price.innerHTML = `$${perMonth[slider.value] * 0.75}.00`;
+  } else {
+    price.innerHTML = `$${perMonth[slider.value]}.00`;
+  }
+}
+
+slider.addEventListener("input", () => {
+  updateValue();
+  console.log(slider.value);
+});
